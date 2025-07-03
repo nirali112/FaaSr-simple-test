31532dac030c8ce91356c795a1c31a270b04d225
Nirali Mehta <61585082+nirali112@users.noreply.github.com>
2025-07-02 19:49:31 -0700

    Add files via upload

---
+++ faasr_simple_test.R
@@ -0,0 +1,9 @@
+
+faasr_simple_test <- function(faasr) {
+  # Simple function that just logs a message
+  cat("Hello from FaaSr! Function is running successfully\n")
+  cat("Current time:", as.character(Sys.time()), "\n")
+  
+  # Return the faasr object (required)
+  return(faasr)
+}
