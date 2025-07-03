9c0c6ec8315fb92a6320cb9d322e659237c72d57
Nirali Mehta <61585082+nirali112@users.noreply.github.com>
2025-07-02 19:52:04 -0700

    Update faasr_simple_test.R

--- faasr_simple_test.R
+++ faasr_simple_test.R
@@ -1,9 +1,15 @@
 
 faasr_simple_test <- function(faasr) {
+  tryCatch({
   # Simple function that just logs a message
   cat("Hello from FaaSr! Function is running successfully\n")
   cat("Current time:", as.character(Sys.time()), "\n")
   
   # Return the faasr object (required)
   return(faasr)
+},
+    error = function(e) {
+    cat("Error occurred:", e$message, "\n")
+    return(faasr)
+  })
 }
